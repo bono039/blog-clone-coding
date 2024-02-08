73473a318748dfd1f799d3fe002a9e4ea958a3c3
bono039 <bono039@hanyang.ac.kr>
2024-02-08 15:05:50 +0900

    Feat: 글 수정/생성용 뷰 컨트롤러, 뷰 생성& 수정 기능 구현

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,3 +14,29 @@ if(deleteBtn) {
         });
     });
 }
+
+
+// 수정 기능
+const modifyBtn = document.getElementById('modify-btn');
+
+if(modifyBtn) {
+    modifyBtn.addEventListener('click', event=> {
+       let params = new URLSearchParams(location.search);
+       let id = params.get('id');
+
+       fetch(`/api/articles/${id}`, {
+          method : 'PUT',
+          headers: {  // headers에 요청 형식 지정
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({    // body에 HTML에 입력한 데이터를 JSON 형식으로 바꿔 보냄
+              title:   document.getElementById('title').value,
+              content: document.getElementById('content').value
+          })
+       })
+       .then(() => {    // 요청 완료 시 마무리 작업
+           alert('수정이 완료되었습니다ㅏ.');
+           location.replace(`/articles/${id}`);
+       });
+    });
+}
