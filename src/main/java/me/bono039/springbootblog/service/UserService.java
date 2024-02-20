fb54b40cfc9b7d160ad346d6a41466f3a7570c20
bono039 <bono039@hanyang.ac.kr>
2024-02-20 21:41:58 +0900

    Feat: OAuth2, JWT에 맞게 설정 파일 작성

--- src/main/java/me/bono039/springbootblog/service/UserService.java
+++ src/main/java/me/bono039/springbootblog/service/UserService.java
@@ -12,19 +12,26 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 // 1) 패스워드 암호화
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
     }
 
     // 유저 ID로 유저 검색해 전달하는 메서드
     public User findById(Long userId) {
         return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    // 이메일 입력받아 users 테이블에서 유저를 찾고, 없으면 예외 발생
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
