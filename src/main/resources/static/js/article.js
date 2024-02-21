5f41012282452dd4a40a3fc4569fc1845eab6532
bono039 <bono039@hanyang.ac.kr>
2024-02-21 20:55:50 +0900

    Fix: 오타 수정

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -2,7 +2,7 @@
 const deleteBtn = document.getElementById('delete-btn');
 
 if(deleteBtn) {
-    deleteBtn.addEventListener('click', (event) => {
+    deleteBtn.addEventListener('click', event => {
         let id = document.getElementById('article-id').value;
 
         function success() {
@@ -23,7 +23,7 @@ if(deleteBtn) {
 const modifyBtn = document.getElementById('modify-btn');
 
 if(modifyBtn) {
-    modifyBtn.addEventListener('click', (event)=> {
+    modifyBtn.addEventListener('click', event=> {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
@@ -47,40 +47,40 @@ if(modifyBtn) {
 }
 
 // 등록(생성) 기능
-const createBtn = document.getElementById("create-btn");
+const createBtn = document.getElementById('create-btn');
 
 if(createBtn) {
     // 등록 버튼 클릭하면 /api/articles로 요청 보냄
-    createBtn.addEventListener("click", (event) => {
+    createBtn.addEventListener('click', event => {
         body = JSON.stringify({
-            title: document.getElementById("title").value,
-            content: document.getElementById("content").value,
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
         });
 
         function success() {
-            alert("등록 완료!");
-            location.replace("/articles");
+            alert('등록 완료!');
+            location.replace('/articles');
         }
 
         function fail() {
-            aler("등록 실패ㅠ");
-            location.replace("/articles");
+            alert('등록 실패ㅠ');
+            location.replace('/articles');
         }
 
-        httpRequest("POST", "/api/articles", body, success, fail);
+        httpRequest('POST', '/api/articles', body, success, fail)
     });
 }
 
 // 쿠키 가져오는 함수
 function getCookie(key) {
     var result = null;
-    var cookie = document.cookie.split(";");
+    var cookie = document.cookie.split(';');
     cookie.some(function (item) {
        item = item.replace(" ", "");
 
        var dic = item.split("=");
 
-       if(key == dic[0]) {
+       if(key === dic[0]) {
            result = dic[1];
            return true;
        }
@@ -99,13 +99,13 @@ function httpRequest(method, url, body, success, fail) {
             "Content-Type": "application/json",
         },
         body: body,
-    }).then((response) => {
-        if(response.status == 200 || response.status == 201) {
+    }).then(response => {
+        if(response.status === 200 || response.status === 201) {
             return success;
         }
 
         const refresh_token = getCookie("refresh_token");
-        if(response.status == 401 && refresh_token) {   // 권한 없다는 에러코드 발생
+        if(response.status === 401 && refresh_token) {   // 권한 없다는 에러코드 발생
             fetch("/api/token", {
                 method: "POST",
                 headers: {
@@ -116,17 +116,17 @@ function httpRequest(method, url, body, success, fail) {
                     refreshToken: getCookie("refresh_token"),
                 }),
             })
-                .then((res) => {
+                .then(res => {
                     if(res.ok) {
                         return res.json();
                     }
                 })
-                .then((result) => {
+                .then(result => {
                     // 재발급 성공하면 로컬 스토리지값을 새로운 액세스 토큰으로 교체
                     localStorage.setItem("access_token", result.accessToken);
                     httpRequest(method, url, body, success, fail);
                 })
-                .catch((error) => fail());
+                .catch(error => fail());
         }
         else {
             return fail();
